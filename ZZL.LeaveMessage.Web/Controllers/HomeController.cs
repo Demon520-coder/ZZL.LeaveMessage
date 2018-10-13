e779336a955a88cb7901ec4fb0837841d4080878
zzlProject <99105337@qq.com>
2018-10-13 23:44:21 +0800

    1.增加日志查看的权限;

--- ZZL.LeaveMessage.Web/Controllers/HomeController.cs
+++ ZZL.LeaveMessage.Web/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using ZZL.LeaveMessage.Common;
 using PagedList;
 using ZZL.LeaveMessage.IService;
+using System.Web.Security;
 
 namespace ZZL.LeaveMessage.Web.Controllers
 {
@@ -22,7 +23,23 @@ namespace ZZL.LeaveMessage.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            //var t = Convert.ToInt32("x");
             var list = _messageService.GetMeesageList();
+
+            FormsAuthenticationTicket ticket = new FormsAuthenticationTicket(1, "token", DateTime.Now, DateTime.Now.AddDays(1), false, "admin");
+
+            HttpCookie cookie = new HttpCookie("ticket");
+            cookie.Value = FormsAuthentication.Encrypt(ticket);
+            cookie.HttpOnly = true;
+            cookie.Expires = DateTime.Now.AddDays(1);
+            var reqCookie = Request.Cookies["ticket"];
+            if (reqCookie != null)
+            {
+                reqCookie.Expires = DateTime.Now.AddYears(-1);
+                Response.Cookies.Add(reqCookie);
+            }
+
+            Response.Cookies.Add(cookie);
 
             return View();
         }
--- ZZL.LeaveMessage.Web/Global.asax.cs
+++ ZZL.LeaveMessage.Web/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using ZZL.LeaveMessage.Common;
 using ZZL.LeaveMessage.Web.App_Start;
 
 namespace ZZL.LeaveMessage.Web
@@ -19,6 +21,43 @@ namespace ZZL.LeaveMessage.Web
             //autofac注入
             AutofacHelper autoFac = new AutofacHelper();
             autoFac.Register();
-        }      
+        }
+
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            if (Request.Url.ToString().Contains("log.axd"))
+            {
+                HttpCookie cookie = Request.Cookies["ticket"];
+                if (cookie == null || cookie.Value.IsNullOrEmpty())
+                {
+                    Response.Redirect("~/BadRequest.html");
+                    Response.End();
+                }
+                else
+                {
+                    //解密:
+                    var ticket = FormsAuthentication.Decrypt(cookie.Value);
+                    //判断是否过期：
+                    if (ticket != null && !ticket.Expired && ticket.Name == "token")
+                    {
+                        //获取用户的身份信息：
+                        if (ticket.UserData != "admin")
+                        {
+                            Response.Redirect("~/BadRequest.html");
+                            Response.End();
+                        }
+                    }
+                    else
+                    {
+                        Response.Redirect("~/BadRequest.html");
+                        Response.End();
+                    }
+
+                }
+
+            }
+                 
+        }
     }
 }
